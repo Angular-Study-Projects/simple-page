fa609d1914a09631a5e51e1da43df5b120b08b49
lpgoulart <leonardo@roffor.com.br>
2019-11-22 09:36:33 -0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
@@ -22,6 +23,7 @@ registerLocaleData(en);
     ContentComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule,
     NgZorroAntdModule,
